6efc577bd91fa287f3354b6317a28d71844244f8
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-28 12:46:15 +0530

    SimpleLnearRegression model

---
+++ Regressions/simpleRegression.py
@@ -0,0 +1,31 @@
+import numpy as np 
+import pandas as pd
+import matplotlib.pyplot as plt
+
+dataset = pd.read_csv("Regressions/Salary_Data.csv")
+X = dataset.iloc[:,:-1].values #independent variable
+y = dataset.iloc[:, -1].values #dependent variable
+
+from sklearn.model_selection import  train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 1)
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression() #create a linear regression object
+regressor.fit(X_train, y_train) #fit the model to the training set
+y_pred = regressor.predict(X_test) #predict the test set
+
+#visualize the training set results
+# plt.scatter(X_train , y_train, color = 'red')
+# plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+# plt.title('Salary vs Experience (Training set)')
+# plt.xlabel('Years of Experience')
+# plt.ylabel('Salary')
+# plt.show()
+
+#visualize the test set results
+plt.scatter(X_test , y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
