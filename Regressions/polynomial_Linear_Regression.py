a08ac34067b6e02d26f1d1038ba2bb58366baf45
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-06-30 11:20:44 +0530

    polynomial regression

--- Regressions/polynomial_Linear_Regression.py
+++ Regressions/polynomial_Linear_Regression.py
@@ -18,4 +18,7 @@ linea_reg.fit(X_poly, Y)
 #visualizing the polynomial linear regression model
 plt.scatter(X, Y, color = 'red')
 plt.plot(X , linea_reg.predict(poly_reg.fit_transform(X)), color = 'blue')
+
+
+print(linea_reg.predict(poly_reg.fit_transform([[8.2]])))  #prediction for a new value in polynomial regression
 plt.show()
