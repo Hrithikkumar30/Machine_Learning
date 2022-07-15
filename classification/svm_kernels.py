4cabdabdb64921aad0fe2a18e7aa4037a98dc07a
Hrithik kumar <hrithik.kumar20@st.niituniversity.in>
2022-07-15 19:39:11 +0530

    svm kernels with visualization

--- classification/svm_kernels.py
+++ classification/svm_kernels.py
@@ -27,13 +27,13 @@ SVM.fit(X_train, Y_train)
 
 y_pred = SVM.predict(X_test)
 
-print(np.concatenate((y_pred.reshape(len(y_pred),1), Y_test.reshape(len(Y_test),1)),1)) #concatenating predicted and actual values
+# print(np.concatenate((y_pred.reshape(len(y_pred),1), Y_test.reshape(len(Y_test),1)),1)) #concatenating predicted and actual values
 
-from sklearn.metrics import confusion_matrix , accuracy_score # accuracy score is used to calculate the accuracy of the model
-cm = confusion_matrix(Y_test,y_pred)
-print(cm)
-ac = accuracy_score(Y_test,y_pred)
-print(ac)
+# from sklearn.metrics import confusion_matrix , accuracy_score # accuracy score is used to calculate the accuracy of the model
+# cm = confusion_matrix(Y_test,y_pred)
+# print(cm)
+# ac = accuracy_score(Y_test,y_pred)
+# print(ac)
 
 
 # Visualising the Training set results
@@ -53,19 +53,19 @@ plt.ylabel('Estimated Salary')
 plt.legend()
 plt.show()
 
-# Visualising the Test set results
-from matplotlib.colors import ListedColormap
-X_set, y_set = sc.inverse_transform(X_test), Y_test
-X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
-                     np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
-plt.contourf(X1, X2, SVM.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
-             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
-plt.xlim(X1.min(), X1.max())
-plt.ylim(X2.min(), X2.max())
-for i, j in enumerate(np.unique(y_set)):
-    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
-plt.title('Kernel SVM (Test set)')
-plt.xlabel('Age')
-plt.ylabel('Estimated Salary')
-plt.legend()
-plt.show()
+# # Visualising the Test set results
+# from matplotlib.colors import ListedColormap
+# X_set, y_set = sc.inverse_transform(X_test), Y_test
+# X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 10, stop = X_set[:, 0].max() + 10, step = 0.25),
+#                      np.arange(start = X_set[:, 1].min() - 1000, stop = X_set[:, 1].max() + 1000, step = 0.25))
+# plt.contourf(X1, X2, SVM.predict(sc.transform(np.array([X1.ravel(), X2.ravel()]).T)).reshape(X1.shape),
+#              alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+# plt.xlim(X1.min(), X1.max())
+# plt.ylim(X2.min(), X2.max())
+# for i, j in enumerate(np.unique(y_set)):
+#     plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1], c = ListedColormap(('red', 'green'))(i), label = j)
+# plt.title('Kernel SVM (Test set)')
+# plt.xlabel('Age')
+# plt.ylabel('Estimated Salary')
+# plt.legend()
+# plt.show()
